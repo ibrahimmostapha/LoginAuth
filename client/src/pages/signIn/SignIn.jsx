d44ab2bd272b2f64a28e752807641fa11bce8272
ibrahimmostapha <ibrahimmoutapha2000@gmail.com>
2023-04-15 00:21:44 +0300

    front-end-v1

--- client/src/pages/signIn/SignIn.jsx
+++ client/src/pages/signIn/SignIn.jsx
@@ -6,11 +6,11 @@ function SignIn() {
       <h2>Sign In</h2>
       <form>
         <div className="container">
-          <label htmlFor="uname"><b>Username</b></label>
-          <input type="text" placeholder="Enter Username" name="uname" required />
+          <label htmlFor="email"><b>Email</b></label>
+          <input type="text" placeholder="Enter Email" name="email" required />
           <label htmlFor="psw"><b>Password</b></label>
           <input type="password" placeholder="Enter Password" name="psw" required />
-          <button type="submit">Login</button>
+          <button type="submit">Sign In</button>
         </div>
       </form>
     </div>
